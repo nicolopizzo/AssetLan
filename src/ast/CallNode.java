b0a9a032d7a9b5ad756175e306a076a8c6cd4321
biancaraimondi <bianca.raimondi2@studio.unibo.it>
2022-04-11 18:26:00 +0200

    call node

--- src/ast/CallNode.java
+++ src/ast/CallNode.java
@@ -1,9 +1,11 @@
 package ast;
 
 import utils.Environment;
+import utils.STEntry;
 import utils.SemanticError;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CallNode implements Node{
 
@@ -11,8 +13,30 @@ public class CallNode implements Node{
     private ArrayList<Node> exp;
     private ArrayList<String> ids;
 
+    private boolean isVariableDeclared(Environment env, String id) {
+        HashMap<String, ArrayList<STEntry>> symTable = env.getSymTable();
+        ArrayList<STEntry> listOfLevels = symTable.get(id);
+        if (symTable.containsKey(id)) {
+            for (STEntry l : listOfLevels) {
+                if (l.getNestLevel() == env.getNestLevel()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public ArrayList<SemanticError> checkSemantics(Environment env) {
-        return null;
+        ArrayList<SemanticError> errors = new ArrayList<>();
+        if (!isVariableDeclared(env, id)) {
+            errors.add(SemanticError.variableNotDeclared(id));
+        }
+        for (String myId : ids){
+            if (isVariableDeclared(env, myId)) {
+                errors.add(SemanticError.variableNotDeclared(myId));
+            }
+        }
+        return errors;
     }
 }
