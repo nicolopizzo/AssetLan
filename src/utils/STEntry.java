81973309d8589a008f1b623927750e892d1d8cd0
Diego Concetti <diegocoonce@gmail.com>
2022-05-11 17:50:06 +0200

    STEntry status

--- src/utils/STEntry.java
+++ src/utils/STEntry.java
@@ -8,11 +8,13 @@ public class STEntry {
     private ArrayList<TypeNode> types = new ArrayList<>();
     private int nestLevel;
     private int offset;
+    private boolean status;
 
     public STEntry(int nl, TypeNode type, int o) {
         types.add(type);
         nestLevel = nl;
         offset = o;
+        status = false;
     }
 
     public STEntry(int nl, ArrayList<TypeNode> types, int o) {
@@ -31,5 +33,13 @@ public class STEntry {
 
     public ArrayList<TypeNode> getTypes() {
         return types;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
