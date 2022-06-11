c3ef61e1bebceb92577fe5e05072e73f5cb3a836
Nicolò Pizzo <nicolopizzo@outlook.com>
2022-06-11 16:34:11 +0200

    decommented code generation

--- src/res/input.assetlan.asm
+++ src/res/input.assetlan.asm
@@ -1,9 +1,14 @@
 push 0
+push 0
+push 0
 push function0
 push function1
 lfp
+push 3
+push 2
+push 1
 lfp
-push -3
+push -5
 lfp
 add
 lw
@@ -13,16 +18,72 @@ halt
 function0:
 cfp
 lra
+push 2
+lfp
+add
+lw
+push -3
+lfp
+lw
+add
+lw
+add
+push -3
+lfp
+lw
+add
+sw
 push 0
+push 2
+lfp
+add
+sw
+lfp
+push 2
+lfp
+add
+lw
 push 0
-beq label0
+push 2
+lfp
+add
+sw
+push 4
+lfp
+add
+lw
 push 0
-b label1
-label0:
+push 4
+lfp
+add
+sw
+push 3
+lfp
+add
+lw
+push 0
+push 3
+lfp
+add
+sw
 push 1
-label1:
+lfp
+add
+lw
+lfp
+lw
+push -4
+lfp
+lw
+add
+lw
+js
 srv
 sra
+pop
+pop
+pop
+pop
 pop
 sfp
 lrv
@@ -33,17 +94,62 @@ function1:
 cfp
 lra
 lfp
+push 3
+lfp
+add
+lw
+push 0
+push 3
+lfp
+add
+sw
+push 2
+lfp
+add
+lw
+push 0
+push 2
+lfp
+add
+sw
+push 1
+lfp
+add
+lw
+push 0
+push 1
+lfp
+add
+sw
+push 1
+lfp
+add
+lw
 lfp
 lw
-push -2
+push -4
 lfp
 lw
 add
 lw
 js
-print
+push -3
+lfp
+lw
+add
+lw
+srvi
+push 0
+push -3
+lfp
+lw
+add
+sw
 srv
 sra
+pop
+pop
+pop
 pop
 sfp
 lrv
